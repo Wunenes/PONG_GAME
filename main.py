3586462dff8ad3de555f469c81ba46ea2caec77a
Nesh Kimathi <munenekimathig@gmail.com>
2025-02-01 14:45:03 +0300

    Improving collision detection

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ screen.screensize(500, 500)
 screen.tracer(0)
 screen.bgcolor("black")
 
-l_paddle = pong_physics.Pong(-315)
-r_paddle = pong_physics.Pong(310)
 scores = scoreboard.Scoreboard()
 
 screen.update()
+
+l_paddle = pong_physics.Pong(-315)
+r_paddle = pong_physics.Pong(310)
+
 screen.listen()
 screen.onkeypress(key="w", fun=l_paddle.go_up)
 screen.onkeypress(key="s", fun=l_paddle.go_down)
@@ -31,7 +33,15 @@ while game_is_on:
     if ball.ycor() > 270 or ball.ycor() < -270:
         ball.bounce_y()
 
-    if ball.distance(r_paddle) < 50 and ball.xcor() > 308 or ball.distance(l_paddle) < 50 and ball.xcor() < -308:
+    if (ball.xcor() > 300 and ball.xcor() < 320 and 
+    ball.ycor() < r_paddle.ycor() + 50 and 
+    ball.ycor() > r_paddle.ycor() - 50):
+        ball.bounce_x()
+    
+    # Left paddle collision    
+    if (ball.xcor() < -300 and ball.xcor() > -320 and 
+    ball.ycor() < l_paddle.ycor() + 50 and 
+    ball.ycor() > l_paddle.ycor() - 50):
         ball.bounce_x()
 
     if ball.xcor() > 330:
